2b302ab5c1359803e47db28b40b3dc3ebf8fb3b0
geoirb <morochev.g@gmail.com>
2021-04-02 01:50:31 +0300

    start http routing

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/coocood/freecache v1.1.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fasthttp/router v1.3.10
 	github.com/go-kit/kit v0.10.0
 	github.com/jmoiron/sqlx v1.3.1
 	github.com/sethvargo/go-password v0.2.0
--- pkg/user/http/transport.go
+++ pkg/user/http/transport.go
@@ -1,1 +1,19 @@
 package http
+
+import (
+	"net/http"
+
+	"github.com/fasthttp/router"
+	"github.com/geoirb/todos/pkg/user"
+)
+
+const (
+	loginURI            = "/login"
+	getUserList         = "/user"
+	registrationUserURI = "/user/registration"
+	createUserURI       = "/user/create"
+)
+
+func Routing(router *router.Router, svc *user.Service){
+	router.Handle(http.MethodPost)
+}
